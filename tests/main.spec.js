d012138e0edba8171e40927fb85e80201511ea13
Dheyson Alves <dheyson.lucas@hotmail.com>
2020-05-27 15:41:47 -0300

    feat: Create some hooks tests

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,30 +1,29 @@
+// // We describe some file or method
+// describe('Main', () => {
+//   describe('Method A', () => {
+//     // We set a context based on a method
+//     context('Case 1', () => {
+//       // It is gonna run the tests
+//       // Skip allow to jump to the next case
+//       it.skip('should happen something', () => {
+//         // What should happen here!
+//         throw new Error('Just an error');
+//       });
+//     });
+//     // It tells to run only this code block
+//     context('Case 2', () => {
+//       it('should happen something', () => {
+//         // What should happen here!
+//         throw new Error('Just an error');
+//       });
 
-// We describe some file or method
-describe('Main', () => {
-  describe('Method A', () => {
-    // We set a context based on a method
-    context('Case 1', () => {
-      // It is gonna run the tests
-      // Skip allow to jump to the next case
-      it.skip('should happen something', () => {
-        // What should happen here!
-        throw new Error('Just an error');
-      });
-    });
-    // It tells to run only this code block
-    context('Case 2', () => {
-      it('should happen something', () => {
-        // What should happen here!
-        throw new Error('Just an error');
-      });
+//       it('should happen ninini', () => {
+//         // What should happen here!
+//       });
+//     });
+//   });
 
-      it('should happen ninini', () => {
-        // What should happen here!
-      });
-    });
-  });
+//   describe('Method B', () => {
 
-  describe('Method B', () => {
-
-  });
-});
+//   });
+// });
