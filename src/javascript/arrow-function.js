f6a9a5d781fdca08d760acc7ac8687dd4087763f
Dheyson Alves <dheyson.lucas@hotmail.com>
2020-05-27 12:26:23 -0300

    feat: Create a test environment

--- src/javascript/arrow-function.js
+++
@@ -1,7 +0,0 @@
-const arr = ['dubling', 'ireland', 'new zealand'];
-
-const result = arr
-                .filter(name => name === 'dubling')
-                .map(name => `I love ${name}`);
-console.log(result);
-
--- src/javascript/dead-zone.js
+++
@@ -1,3 +0,0 @@
-console.log(message);
-// referenceError
-const message = 4;
