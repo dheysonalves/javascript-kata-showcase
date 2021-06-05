273a1ec313e665581eeb00a93d6b58474147b36a
Dheyson Alves <dheyson.lucas@hotmail.com>
2021-06-05 11:29:58 -0300

    feat: add a testing case for an scenario

---
+++ tests/LessThanGreaterThan/main.spec.js
@@ -0,0 +1,24 @@
+import { expect } from 'chai';
+import ArrayBetween from '../../src/LessThanGreaterThan/main';
+
+describe('Array between two numbers', () => {
+  // it('should return the file', () => {
+  //   expect(ArrayBetween).to.exist;
+  // });
+
+  it('should return an empty array', () => {
+    expect(ArrayBetween(7, 32, [1, 2, 3, 78])).to.be.an('array').that.is.empty;
+  });
+
+  it('should return an array with `[1, 2, 3]`', () => {
+    expect(ArrayBetween(0, 9, [1, 2, 3, 78])).to.be.an('array').that.includes(1, 2, 3);
+  });
+
+  it('should return an array with `[0, 0, -3]`', () => {
+    expect(ArrayBetween(-5, 3, [-8, 0, 0, 20, -3])).to.be.an('array').that.includes(0, 0, -3);
+  });
+
+  it('should return an array with `[0, 0, -3]`', () => {
+    expect(ArrayBetween(1, 1, [1, 0, 3, 7])).to.be.an('array').that.is.empty;
+  });
+});
