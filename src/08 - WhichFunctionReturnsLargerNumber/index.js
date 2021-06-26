3b0488291168cd647b6b2e1fe6fa812ec865e03c
Dheyson Alves <dheyson.lucas@hotmail.com>
2021-06-26 18:15:49 -0300

    refactor: chaging variables and simplified function

--- src/08 - WhichFunctionReturnsLargerNumber/index.js
+++ src/08 - WhichFunctionReturnsLargerNumber/index.js
@@ -1,16 +1,6 @@
 const whichIsLarger = ((callback, callbackTwo) => {
-  const returnCallback = callback();
-  const returnCallbackTwo = callbackTwo();
-
-  if (returnCallback < returnCallbackTwo) {
-    return 'g';
-  }
-
-  if (returnCallback > returnCallbackTwo) {
-    return 'f';
-  }
-
-  return 'neither';
+  if (callback() === callbackTwo()) return 'neither';
+  return (callback() > callbackTwo()) ? 'f' : 'g';
 });
 
 export default whichIsLarger;
