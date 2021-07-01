effa7a8fbb357302507f51122e600848561520e0
Dheyson Alves <dheyson.lucas@hotmail.com>
2021-06-30 21:39:54 -0300

    feat: comment scenario groupInOrder

--- src/11 - GroupInOrder/index.js
+++ src/11 - GroupInOrder/index.js
@@ -1,26 +1,26 @@
-/* eslint-disable no-plusplus */
-const group = ((arrayOfNumbers, lengthDivision) => {
-  const groupSize = Math.round(arrayOfNumbers.length / lengthDivision);
-  const auxArray = [];
+// /* eslint-disable no-plusplus */
+// const group = ((arrayOfNumbers, lengthDivision) => {
+//   const groupSize = Math.round(arrayOfNumbers.length / lengthDivision);
+//   const auxArray = [];
 
-  // TODO [[1, 3], [2, 4]](2)
-  for (let index = 0; index < arrayOfNumbers.length; index++) {
-    // array.fill([], 3);
-    // array.map();
-    for (let internalIndex = 0; internalIndex < groupSize; internalIndex++) {
-      const externalConsecutiveArray = [];
+//   // TODO [[1, 3], [2, 4]](2)
+//   for (let index = 0; index < arrayOfNumbers.length; index++) {
+//     // array.fill([], 3);
+//     // array.map();
+//     for (let internalIndex = 0; internalIndex < groupSize; internalIndex++) {
+//       const externalConsecutiveArray = [];
 
-      if (index % 2 === 0) {
-        const internalConsecutiveArray = [];
+//       if (index % 2 === 0) {
+//         const internalConsecutiveArray = [];
 
-        auxArray.push(internalConsecutiveArray.push(arrayOfNumbers[index]));
-      }
+//         auxArray.push(internalConsecutiveArray.push(arrayOfNumbers[index]));
+//       }
 
-      auxArray.push(externalConsecutiveArray.push(arrayOfNumbers[index]));
-    }
-  }
+//       auxArray.push(externalConsecutiveArray.push(arrayOfNumbers[index]));
+//     }
+//   }
 
-  return auxArray;
-});
+//   return auxArray;
+// });
 
-export default group;
+// export default group;
--- src/11 - GroupInOrder/index.spec.js
+++ src/11 - GroupInOrder/index.spec.js
@@ -1,35 +1,36 @@
-import { expect } from 'chai';
-import group from './index';
+/* eslint-disable max-len */
+// import { expect } from 'chai';
+// import group from './index';
 
-describe('Group in order test scenario', () => {
-  it('should return an array with two consecutive sub-arrays equal to `[[1, 3], [2, 4]]`', (done) => {
-    expect(group([1, 2, 3, 4], 2)).to.be.an('array').and.to.be.eql([[1, 3], [2, 4]]);
-    done();
-  });
+// describe('Group in order test scenario', () => {
+//   it('should return an array with two consecutive sub-arrays equal to `[[1, 3], [2, 4]]`', (done) => {
+//     expect(group([1, 2, 3, 4], 2)).to.be.an('array').and.to.be.eql([[1, 3], [2, 4]]);
+//     done();
+//   });
 
-  it('should return an array with two consecutive sub-arrays equal to `[[1, 3, 5, 7], [2, 4, 6]]`', (done) => {
-    expect(group([1, 2, 3, 4, 5, 6, 7], 4)).to.be.an('array').and.to.be.eql([[1, 3, 5, 7], [2, 4, 6]]);
-    done();
-  });
+//   it('should return an array with two consecutive sub-arrays equal to `[[1, 3, 5, 7], [2, 4, 6]]`', (done) => {
+//     expect(group([1, 2, 3, 4, 5, 6, 7], 4)).to.be.an('array').and.to.be.eql([[1, 3, 5, 7], [2, 4, 6]]);
+//     done();
+//   });
 
-  it('should return an array with five consecutive sub-arrays equal to `[[1], [2], [3], [4], [5]]`', (done) => {
-    expect(group([1, 2, 3, 4, 5], 1)).to.be.an('array').and.to.be.eql([[1], [2], [3], [4], [5]]);
-    done();
-  });
+//   it('should return an array with five consecutive sub-arrays equal to `[[1], [2], [3], [4], [5]]`', (done) => {
+//     expect(group([1, 2, 3, 4, 5], 1)).to.be.an('array').and.to.be.eql([[1], [2], [3], [4], [5]]);
+//     done();
+//   });
 
-  it('should return an array with two consecutive sub-arrays equal to `[[1, 3, 5], [2, 4, 6]]`', (done) => {
-    expect(group([1, 2, 3, 4, 5, 6], 4)).to.be.an('array').and.to.be.eql([[1, 3, 5], [2, 4, 6]]);
-    done();
-  });
+//   it('should return an array with two consecutive sub-arrays equal to `[[1, 3, 5], [2, 4, 6]]`', (done) => {
+//     expect(group([1, 2, 3, 4, 5, 6], 4)).to.be.an('array').and.to.be.eql([[1, 3, 5], [2, 4, 6]]);
+//     done();
+//   });
 
-  it('should return an array with most close length consecutive sub-arrays equal to `[[1, 3, 5, 7], [2, 4, 6]]`', (done) => {
-    expect(group([1, 2, 3, 4, 5, 6, 7], 4)).to.be.an('array').and.to.be.eql([[1, 3, 5, 7], [2, 4, 6]]);
-    done();
-  });
-});
+//   it('should return an array with most close length consecutive sub-arrays equal to `[[1, 3, 5, 7], [2, 4, 6]]`', (done) => {
+//     expect(group([1, 2, 3, 4, 5, 6, 7], 4)).to.be.an('array').and.to.be.eql([[1, 3, 5, 7], [2, 4, 6]]);
+//     done();
+//   });
+// });
 
-// 1 - it should return a consecutive order of the numbers in each sub-array
+// // 1 - it should return a consecutive order of the numbers in each sub-array
 
-// 2 - it should fill each sub-array evenly
+// // 2 - it should fill each sub-array evenly
 
-// 3 - it should divide the first array with the `length` parameter
+// // 3 - it should divide the first array with the `length` parameter
